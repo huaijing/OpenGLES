2c564a8a4a133deb589066022e683ec7097d8462
淮静 <huaijing@bytedance.com>
2016-10-09 11:03:47 +0800

    传入textureVertices

--- Tutorial03/Tutorial03/VertexShader.glsl
+++ Tutorial03/Tutorial03/VertexShader.glsl
@@ -3,11 +3,13 @@ uniform mat4 modelView;
 attribute vec4 vPosition; 
 
 attribute vec2 vTextureCoord;
+
 varying vec2 vTextureCoordOut;
 
 void main(void)
 {
-    gl_Position = projection * modelView * vPosition;
+//    gl_Position = projection * modelView * vPosition;
     
+    gl_Position = vPosition;
     vTextureCoordOut = vTextureCoord;
 }
